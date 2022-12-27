d8600b13fb79a1aa94439f2e4f559a60bcccfdf5
MatheusSilvaMendes <matheussm.090@gmail.com>
2022-12-27 20:31:22 -0300

    ajustes repositório

---
+++ src/EstruturaDeRepeticao.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class EstruturaDeRepeticao {
+
+	public static void main(String[] args) {
+		
+//		utilizando o while
+		int contador = 0;
+		while (contador <= 10) {
+			System.out.println("Repetição While " + contador + " de 10");
+			contador++;
+		}
+		
+		System.out.println("\n-------------------------------");
+	}
+}
